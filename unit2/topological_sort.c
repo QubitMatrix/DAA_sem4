23f7fe78bf8e928ecb1facc4045b0c9841bd9e03
Preethi <preethi13m@gmail.com>
2023-02-15 00:22:09 +0530

    added cycle detection to topological sort implementation using bfs

--- unit2/topological_sort.c
+++ unit2/topological_sort.c
@@ -1,25 +1,33 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void dfs(int** graph,int* visited,int* deadendorder,int n,int src,int* vorder,int* top)
+void dfs(int** graph,int* visited,int* deadendorder,int n,int src,int* vorder,int* top,int* instack)
 {
 	(*vorder)++;
 	visited[src]=*vorder;
+	instack[src]=1;
 	for(int i=0;i<n;i++)
 	{
 		if(graph[src][i]==1 && visited[i]==0)
-			dfs(graph,visited,deadendorder,n,i,vorder,top);
+			dfs(graph,visited,deadendorder,n,i,vorder,top,instack);
+		else if(graph[src][i]==1 && visited[i]!=0 && instack[i]==1)
+		{
+			printf("DAG Error, topological sort cannot be performed\n");
+			exit(1);
+		}
+
 	}
+	instack[src]=0;
 	deadendorder[(*top)++]=src;
 }
-void DFS(int** graph,int* visited,int* deadendorder,int n)
+void DFS(int** graph,int* visited,int* deadendorder,int n,int* instack)
 {
 	int vorder=0;
 	int top=0;
 	for(int i=0;i<n;i++)
 	{
 		if(visited[i]==0)
-			dfs(graph,visited,deadendorder,n,i,&vorder,&top);
+			dfs(graph,visited,deadendorder,n,i,&vorder,&top,instack);
 	}
 }
 int main()
@@ -40,7 +48,8 @@ int main()
 	}
 	int* visited=calloc(n,sizeof(int));
 	int* deadendorder=calloc(n,sizeof(int));
-	DFS(graph,visited,deadendorder,n);
+	int* instack=calloc(n,sizeof(int));
+	DFS(graph,visited,deadendorder,n,instack);
 	printf("Order of visiting:\n");
 	for(int i=0;i<n;i++)
 	{
